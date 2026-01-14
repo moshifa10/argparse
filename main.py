b12931bba87c2d111ead38aedc97168d8866c13a
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-01-14 12:02:10 +0200

    just got started with the cli using argparse

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+
+# I will be learning about the argparse 
+
+# parser = argparse.ArgumentParser(description="learning about argparse")
+# parser.add_argument("-name", help="enter your name")
+
+
+# args = parser.parse_args()
+# print(f"Your name: {args.name}")
+
+# challange1
+
+# parser = argparse.ArgumentParser(description="Takes your name")
+# parser.add_argument('name', help="Your name")
+# parser.add_argument('-u','--upper',action="store_true", help="This just turns your things to upper case")
+
+# args = parser.parse_args()
+# if args.upper:
+#     print(f"HELLO {args.name.upper()}")
+# else:
+#     print(f"Hello {args.name}")
+
+
+# 🟢 Challenge 2 — EASY
+
+# I will create a cli that takes a number as your age
+# parser = argparse.ArgumentParser(description="I will creat a CLI for taking age")
+# parser.add_argument('--age', type=int, required=True, help="This is your age")
+
+# args = parser.parse_args()
+# print(f"You are {args.age} years old.")
+
+
+# 🟡 Challenge 3 — EASY ➜ MEDIUM
